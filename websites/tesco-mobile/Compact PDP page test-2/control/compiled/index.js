a6aa0865e041cfb268a0d578ace048ccf501cf3e
khalek-echologyx <khalek@echologyx.com>
2026-01-30 15:42:17 +0600

    Variation B completed

---
+++ websites/tesco-mobile/Compact PDP page test-2/control/compiled/index.js
@@ -0,0 +1,47 @@
+var id = "1769681941358_5496_control";
+var name = "control";
+var testInfo = {
+	id: id,
+	name: name};
+
+(() => {
+  function waitForElem(
+    waitFor,
+    callback,
+    minElements = 1,
+    isVariable = false,
+    timer = 10000,
+    frequency = 25
+  ) {
+    let elements = isVariable
+      ? window[waitFor]
+      : document.querySelectorAll(waitFor);
+    if (timer <= 0) return;
+    (!isVariable && elements.length >= minElements) ||
+    (isVariable && typeof window[waitFor] !== "undefined")
+      ? callback(elements)
+      : setTimeout(
+          () =>
+            waitForElem(
+              waitFor,
+              callback,
+              minElements,
+              isVariable,
+              timer - frequency
+            ),
+          frequency
+        );
+  }
+
+  function mainJs([body]) {
+    console.table({ ID: testInfo.id, Variation: testInfo.name });
+
+    console.log(
+      "%cname: v-01",
+      "background: black;border: 2px solid green;color: white;display: block;text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3);text-align: center;font-weight: bold;padding : 10px;margin : 10px"
+    );
+    console.log("name: v-01");
+  }
+
+  waitForElem("body", mainJs);
+})();
